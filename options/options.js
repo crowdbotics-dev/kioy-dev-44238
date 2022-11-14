b8a659c6154c13d982dbee9a7940cb6dd691e04a
Crowdbotics <engineering@crowdbotics.com>
2022-11-14 05:44:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kioy-dev-44238",
+  url: "https://kioy-dev-44238.botics.co",
+  api: "https://kioy-dev-44238.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
